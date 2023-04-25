f8529d7cb2ee09eec8a775a47e888c5d9b7c47e1
Amanda Alves <amandagsal@gmail.com>
2023-04-25 17:10:04 -0300

    Implementa configurações e cliente soap

--- src/main/java/com/javatechie/spring/soap/api/SpringBootSoapWsClientApplication.java
+++ src/main/java/com/javatechie/spring/soap/api/SpringBootSoapWsClientApplication.java
@@ -1,10 +1,26 @@
 package com.javatechie.spring.soap.api;
 
+import com.javatechie.spring.soap.api.client.SoapClient;
+import com.javatechie.spring.soap.api.loaneligibility.Acknowledgement;
+import com.javatechie.spring.soap.api.loaneligibility.CustomerRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootSoapWsClientApplication {
+
+	@Autowired
+	private SoapClient client;
+
+	@PostMapping("/getLoanStatus")
+	public Acknowledgement invokeSoapClientToGetLoanStatus(@RequestBody CustomerRequest request){
+		return client.getLoanStatus(request);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSoapWsClientApplication.class, args);
